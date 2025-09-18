87f985b86074eff3f60eefa9996d9d0f2c1877ce
Mohaddisa Zehra <k250663@nu.edu.pk>
2025-09-18 12:54:49 +0500

    Update 2.c

--- 2.c
+++ 2.c
@@ -42,18 +42,85 @@ int main(){
 				
 					break;
 			}
-		
-		
 }
 			break;
 			case 2:
 			printf("chosen dept : medicine");
+			printf("\n1.mbbs \n 2.bds \n choose:");
+			scanf("%d",&dept);
+			switch(dept){
+				case 1:
+					printf("chosen dept : mbbs");
+		
+					printf("\n1.lab \n 2.theory \n choose:");
+					scanf("%d", &dept);
+					switch(dept){
+						case 1:
+							printf("chosen : lab");
+							break;
+						case 2:
+							printf("chosen:theory");
+							break;
+						default:
+							printf("invalid");
+					}
+					break;
+				case 2:
+					printf("chosen dept :bds");
+					printf("\n1.lab \n 2.theory \n choose:");
+					scanf("%d", &dept);
+					switch(dept){
+						case 1:
+							printf("chosen : lab");
+							break;
+						case 2:
+							printf("chosen:theory");
+							break;
+						default:
+							printf("invalid");
+		}
+	}
 			break;
 			case 3:
 			printf("chosen dept : business");
+			printf("\n1.bba \n 2.acf \n choose:");
+			scanf("%d",&dept);
+			switch(dept){
+				case 1:
+					printf("chosen dept : bba");
+		
+					printf("\n1.lab \n 2.theory \n choose:");
+					scanf("%d", &dept);
+					switch(dept){
+						case 1:
+							printf("chosen : lab");
+							break;
+						case 2:
+							printf("chosen:theory");
+							break;
+						default:
+							printf("invalid");
+					}
+					break;
+				case 2:
+					printf("chosen dept :acf");
+					printf("\n1.lab \n 2.theory \n choose:");
+					scanf("%d", &dept);
+					switch(dept){
+						case 1:
+							printf("chosen : lab");
+							break;
+						case 2:
+							printf("chosen:theory");
+							break;
+						default:
+							printf("invalid");
 			break;
-			default:
+		
 			printf("invalid");
 	}
-	
 }
+}
+}
+
+	
